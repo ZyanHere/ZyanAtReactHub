ef975ac4d8b7ae3b25822a2c644913a1f3152665
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-01-22 22:33:09 +0530

    day1

---
+++ app1/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav>
+        <h1>TechStar</h1>
+        <main>
+            <Link to= {"/"}>Home</Link>
+            <Link to= {"/contact"}>contact</Link>
+            <Link to= {"/#about"}>about</Link>
+            <Link to= {"/#brands"}>Brands</Link>
+            <Link to= {"/services"}>Services</Link>
+
+        </main>
+    </nav>
+  )
+}
+
+export default Header
