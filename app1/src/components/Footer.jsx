07b13ed84598b457d0178f5e576cdba94d43fabd
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-01 21:39:44 +0530

    footer and contact

---
+++ app1/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.facebook.com/znyandeep.baishya" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://www.linkedin.com/in/jyan-baishya-a6a502217/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/ZyanHere" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
