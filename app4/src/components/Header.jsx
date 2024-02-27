3c5ed95cd6db818d98e756bfcd921c1c3060528a
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-27 21:12:42 +0530

    styles and headers updated

--- app4/src/components/Header.jsx
+++ app4/src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <h2>Logo Here.</h2>
 
-export default Header
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/cart"}>
+          <FiShoppingBag />
+          <p>{0}</p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
