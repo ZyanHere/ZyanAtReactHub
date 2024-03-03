5f6bf158b5cde316f71dacae01e728fadbf5c879
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-03-03 21:15:03 +0530

    header updated

--- app5/src/components/Header.jsx
+++ app5/src/components/Header.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <NavContent/>
+    </nav>
   )
 }
+
+const NavContent = () => (
+  <>
+    <h2>Zyan</h2>
+    <div>
+      <a  href="#home">
+        Home
+      </a>
+      <a  href="#work">
+        Work
+      </a>
+      <a  href="#timeline">
+        Experience
+      </a>
+      <a  href="#services">
+        Services
+      </a>
+      <a  href="#testimonial">
+        Testimonial
+      </a>
+      <a  href="#contact">
+        Contact
+      </a>
+    </div>
+    <a href="maillto:craftzyan@gmail.com">
+      <button>Email</button>
+    </a>
+  </>
+);
 
 export default Header
