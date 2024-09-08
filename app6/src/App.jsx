80d997ef5edb74c61035f174442966772c1e5f3e
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-09-08 23:07:25 +0530

    hero section updated

--- app6/src/App.jsx
+++ app6/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import HeroImg from './assets/hero.jpg'
+
+const bgImage = {
+  backgroundImage: `url(${HeroImg})`,
+  backgroundSize: 'cover',
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat",
+  position: "relative",
+}
 
 const App = () => {
   return (
     <div className='overflow-x-hidden bg-brandDar text-white'>
-      <Navbar/>
+      <div style={bgImage}>
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   )
 }
---
+++ app6/src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaPlay} from "react-icons/fa";
+import { motion } from "framer-motion"
+
+const Hero = () => {
+  return (
+    <>
+        <section className='min-h-[900px] flex justify-center items-center'>
+            {/* radial gradient section */}
+            <div className='absolute inset-0 bg-gradient-radial from-transparent via-black/50 to-black z-10'
+            style={{
+                background: "radial-gradient(circle, rgba(0,0,0,0.3) 50%, rgba(11,11,13,0.5) 70%, rgba(11,11,13,0.8) 90%)",}}></div>
+
+            {/* hero text section */}
+            <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} transition={{ duration: 1.5, delay:0.5}} className='container relative z-20 text-center space-y-8'>
+                <h1 className='text-5xl md:text-8xl font-bold font-merriweather'>The Great Outdoors</h1>
+                <p>Discover the wonders of nature</p>
+                <div className='inline-flex justify-center items-center border-2 h-1/4 w-14 rounded-full !mt-14 md:!mt-20'>
+                    <FaPlay/>
+                </div>
+            </motion.div>
+        </section>
+    </>
+  )
+}
+
+export default Hero
